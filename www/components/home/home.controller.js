8100ad14054ce6f018574bc610384340ad33f9c3
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-11-02 11:47:57 -0600

    Added controller functions from codepen example

--- www/components/home/home.controller.js
+++ www/components/home/home.controller.js
@@ -18,7 +18,6 @@
     function login() {
       authService.login()
     }
-
     //API queries inserted here
     var combinedArray = [];
     var searchUrl = "https://api.foursquare.com/v2/venues/explore?ll=39.74,-104.99&client_id=NHF0X5EXQLHYJ3IG5FIYSJYD2R33BLQSKGGQUBSIYMXWFYA4&client_secret=5TRQLKFODOFFJW55T0FHBH3BWNW3RFAOBK24BK2BSPB2QD3C&v=20161031&section=food";
@@ -29,17 +28,27 @@
           var venueSearchUrl = "https://api.foursquare.com/v2/venues/"+elem.venue.id+"/photos?client_id=NHF0X5EXQLHYJ3IG5FIYSJYD2R33BLQSKGGQUBSIYMXWFYA4&client_secret=5TRQLKFODOFFJW55T0FHBH3BWNW3RFAOBK24BK2BSPB2QD3C&v=20161031&section=food"
           $http.get(venueSearchUrl)
           .then(function(venuePicsUrl) {
-            var final = {}
+            var final = {};
             final.id = elem.venue.id;
             final.name = elem.venue.name;
             final.hours = elem.venue.hours;
-            final.pic = venuePicsUrl.data.response.photos.items[1].prefix+'300x500'+venuePicsUrl.data.response.photos.items[1].suffix;
-            combinedArray.push(final)
+            final.image = venuePicsUrl.data.response.photos.items[1].prefix+'300x500'+venuePicsUrl.data.response.photos.items[1].suffix;
+            combinedArray.push(final);
           })
         })
     })
     console.log(combinedArray);
-    $scope.view.restaurants = combinedArray
+    $scope.view.restaurants = combinedArray;
+    $scope.cards = Array.prototype.slice.call($scope.view.restaurants, 0);
+    $scope.cardDestroyed = function(index) {
+      $scope.cards.splice(index, 1);
+    };
+    
+    $scope.addCard = function() {
+      var newCard = $scope.view.restaurants[Math.floor(Math.random() * $scope.view.restaurants.length)];
+      newCard.id = Math.random();
+      $scope.cards.push(angular.extend({}, newCard));
+    }
   }
 
 }());
