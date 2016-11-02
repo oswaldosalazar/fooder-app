fb3dc83f8b9df2737bbd5a964a0b4f876c5d4b06
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-11-02 16:07:35 -0600

    Added venue name, hours and address

--- www/components/home/home.controller.js
+++ www/components/home/home.controller.js
@@ -20,19 +20,20 @@
     }
     //API queries inserted here
     var cardTypes = [];
-    var searchUrl = "https://api.foursquare.com/v2/venues/explore?ll=39.74,-104.99&client_id=NHF0X5EXQLHYJ3IG5FIYSJYD2R33BLQSKGGQUBSIYMXWFYA4&client_secret=5TRQLKFODOFFJW55T0FHBH3BWNW3RFAOBK24BK2BSPB2QD3C&v=20161031&section=food";
+    var searchUrl = "https://api.foursquare.com/v2/venues/explore?ll=39.74,-104.99&client_id=NHF0X5EXQLHYJ3IG5FIYSJYD2R33BLQSKGGQUBSIYMXWFYA4&client_secret=5TRQLKFODOFFJW55T0FHBH3BWNW3RFAOBK24BK2BSPB2QD3C&v=20161031&section=food&openNow=1";
     $http.get(searchUrl)
     .then(function(restaurants){
         var results = restaurants.data.response.groups[0].items;
         results.map(function(elem){
-          var venueSearchUrl = "https://api.foursquare.com/v2/venues/"+elem.venue.id+"/photos?client_id=NHF0X5EXQLHYJ3IG5FIYSJYD2R33BLQSKGGQUBSIYMXWFYA4&client_secret=5TRQLKFODOFFJW55T0FHBH3BWNW3RFAOBK24BK2BSPB2QD3C&v=20161031&section=food"
+          var venueSearchUrl = "https://api.foursquare.com/v2/venues/"+elem.venue.id+"/photos?client_id=NHF0X5EXQLHYJ3IG5FIYSJYD2R33BLQSKGGQUBSIYMXWFYA4&client_secret=5TRQLKFODOFFJW55T0FHBH3BWNW3RFAOBK24BK2BSPB2QD3C&v=20161031"
           $http.get(venueSearchUrl)
           .then(function(venuePicsUrl) {
             var final = {};
             final.id = elem.venue.id;
             final.name = elem.venue.name;
             final.hours = elem.venue.hours;
-            final.image = venuePicsUrl.data.response.photos.items[0].prefix+'300x500'+venuePicsUrl.data.response.photos.items[0].suffix;
+            final.address = elem.venue.location.address;
+            final.image = venuePicsUrl.data.response.photos.items[2].prefix+'300x400'+venuePicsUrl.data.response.photos.items[2].suffix;
             cardTypes.push(final);
           })
         })
